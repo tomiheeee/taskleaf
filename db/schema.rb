b26b0be8ad05821dc32d43cc81ddff4f7ebe095e
Yuhei Tomita <tomita.yuhei@moneyforward.co.jp>
2019-06-09 19:11:53 +0900

    NULL制約とnameの文字数を制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_004308) do
+ActiveRecord::Schema.define(version: 2019_06_09_100736) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
